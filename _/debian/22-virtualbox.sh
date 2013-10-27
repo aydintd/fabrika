bb1014d9b949d1b2b0bcaa910dc0e1e6ae537118
Recai Oktaş <roktas@bil.omu.edu.tr>
2013-10-27 22:14:40 +0200

    virtualbox eklenti kurulumunu düzelt

--- _/debian/22-virtualbox.sh
+++ _/debian/22-virtualbox.sh
@@ -2,7 +2,9 @@
 
 say() { echo -e >&2 "\033[36;01m${*}\033[0m"; }
 
-[ -f .vbox_version ] || exit 0
+vbox_version_file=/home/vagrant/.vbox_version
+
+[ -f "$vbox_version_file" ] || exit 0
 
 say install virtualbox guest additions
 
@@ -17,11 +19,11 @@ apt-get -y --no-install-recommends install \
 	linux-headers-$(uname -r) dkms build-essential
 apt-get -y --no-install-recommends install libdbus-1-3
 
-mount -o loop VBoxGuestAdditions.iso /mnt
+mount -o loop VBoxGuestAdditions_$(cat "$vbox_version_file").iso /mnt
 yes|sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 rm -f VBoxLinuxAdditions.iso
 
 /etc/init.d/vboxadd start
 
-apt-get -y purge linux-headers-$(uname -r) dkms build-essential
+apt-get -y purge linux-headers-$(uname -r) build-essential
